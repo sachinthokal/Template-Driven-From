87d8680f84fb159c633f88b5edac6acb4e222549
sachinthokal <sachinthokal1799@gmail.com>
2022-12-24 15:13:20 +0530

    From Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Template-Driven-Form';
+
+  Username: string = 'Sachin';
+  Password: string = '12345';
+  Mobile: number = 88171399;
+
+  onFormSubmit(userForm: NgForm) {
+    console.log(userForm.value);
+    this.Username = userForm.controls['username'].value;
+    this.Password = userForm.controls['Pass'].value;
+    this.Mobile = userForm.controls['Mob'].value;
+  }
 }
